70aee3aebb82e492265f116c89bb0ae1b83a263a
harayu <harayu1212@gmail.com>
2024-10-01 12:29:48 +0900

    7章完了

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { notFound } from "next/navigation";
+import NewsList from "@/app/_components/NewsList";
+import {
+  getNewsDetail,
+  getNewsList,
+  getCategoryDetail,
+} from "@/app/_libs/microcms";
+import Article from "@/app/_components/Article";
+import ButtonLink from "@/app/_components/ButtonLink";
+import styles from "./page.module.css";
+import Category from "@/app/_components/Category";
+import Pagination from "@/app/_components/Pagination";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const category = await getCategoryDetail(params.id).catch(notFound);
+
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+    filters: `category[equals]${category.id}`,
+  });
+
+  return (
+    <>
+      <p>
+        <Category category={category} />
+        の一覧
+      </p>
+      <NewsList news={news} />
+      <Pagination
+        totalCount={totalCount}
+        basePath={`/news/category/${category.id}`}
+      />
+    </>
+  );
+}
